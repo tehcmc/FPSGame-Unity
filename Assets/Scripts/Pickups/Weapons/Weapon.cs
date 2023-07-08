af023b81ffc4685f8fb6043b26ac065a38730cc5
Craig C <craigcostello@btinternet.com>
2023-07-08 21:00:47 +0100

    Added weapon fire effects, begin adding enemy animations

--- Assets/Scripts/Pickups/Weapons/Weapon.cs
+++ Assets/Scripts/Pickups/Weapons/Weapon.cs
@@ -7,6 +7,24 @@ public class Weapon : MonoBehaviour
 	[SerializeField] Camera cam;
 	[SerializeField] float bulletRange = 100f;
 	[SerializeField] float bulletDamage = 10f;
+	[SerializeField] int maxAmmo = 15;
+
+	[SerializeField] Transform muzzle;
+	[SerializeField] ParticleSystem muzzleFlash;
+	[SerializeField] TrailRenderer bulletTrail;
+
+	[SerializeField] ParticleSystem hitParticle;
+
+	public bool CanFire()
+	{
+
+		if (!Input.GetButtonDown("Fire1")) return false;
+
+		return true;
+
+
+	}
+
 	void Update()
 	{
 		if (CanFire())
@@ -17,32 +35,54 @@ public class Weapon : MonoBehaviour
 
 	void Fire()
 	{
+
 		RaycastHit tr;
+		Vector3 hitLoc = cam.transform.forward * bulletRange;
+
 		Debug.DrawLine(cam.transform.position, cam.transform.forward * bulletRange, Color.red);
+
 		if (Physics.Raycast(cam.transform.position, cam.transform.forward, out tr, bulletRange))
 		{
-			if (tr.transform != transform.parent)
+			if (tr.transform == transform.parent) return;
+			hitLoc = tr.point;
+
+			DoImpactEffect(tr);
+
+			var health = tr.collider.gameObject.GetComponent<Health>();
+			if (health)
 			{
-				var health = tr.collider.gameObject.GetComponent<Health>();
-				if (health)
-				{
-					health.ModifyHealth(-bulletDamage);
-				}
-				Debug.Log(tr.collider.name);
+				health.ModifyHealth(-bulletDamage);
+
 			}
 
-		}
+			Debug.Log(tr.collider.name);
 
+
+		}
+		TrailRenderer trail = Instantiate(bulletTrail, muzzle.position, Quaternion.identity);
+		var muzzleEffect = Instantiate(muzzleFlash, muzzle.transform);
+		muzzleEffect.transform.position = muzzle.position; muzzleEffect.transform.rotation = muzzle.rotation;
+		StartCoroutine(SpawnTrail(trail, hitLoc));
+	}
+	void DoImpactEffect(RaycastHit tr)
+	{
+
+		Instantiate(hitParticle, tr.point, Quaternion.LookRotation(tr.normal));
 
 
 	}
-	public bool CanFire()
+	IEnumerator SpawnTrail(TrailRenderer trail, Vector3 hitLoc)
 	{
-		if (!Input.GetButtonDown("Fire1")) return false;
-
-
-
-		return true;
-
+		float time = 0;
+		Vector3 startPos = trail.transform.position;
+		while (time < 1)
+		{
+			trail.transform.position = Vector3.Lerp(startPos, hitLoc, time);
+			time += Time.deltaTime / trail.time;
+			yield return null;
+		}
+		trail.transform.position = hitLoc;
+		Destroy(trail.gameObject);
 	}
 }
+
